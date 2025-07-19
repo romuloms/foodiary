9db271f80b868a4e2e2aa27404cff2bcf78780f3
Romulo Santana <romulomsantana@hotmail.com>
2025-07-19 11:33:36 -0300

    add controllers, functions, types and utils

--- src/signup.ts
+++
@@ -1,8 +0,0 @@
-export async function handler(event) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            hello: 'Funcionou! Criando a conta...',
-        }),
-    }
-}
